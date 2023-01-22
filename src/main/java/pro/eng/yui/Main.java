b8ea0a720635ab22199ffcaa0bf4df27ee891eaf
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-01-22 13:54:17 +0900

    first commit

---
+++ src/main/java/pro/eng/yui/Main.java
@@ -0,0 +1,11 @@
+package pro.eng.yui;
+
+/**
+ *
+ * @author KITAMURA Yui
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("©Yui-Kitamura LLC.");
+    }
+}
